f3446f0e24cdaf0e73151b7c70be04167c9b9414
Smile2578 <107757684+Smile2578@users.noreply.github.com>
2023-06-30 15:37:56 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { BiUser } from 'react-icons/bi';
 import { FiLogOut } from 'react-icons/fi';
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineUserAdd} from 'react-icons/ai';
+import { FaFileDownload } from 'react-icons/fa';
 import { HiMenu } from 'react-icons/hi';
 import { useRouter } from 'next/router';
 import { useAuth } from '../AuthContext';
@@ -54,20 +55,20 @@ export default function Dashboard({ children }) {
     <div className="min-h-screen flex flex-col items-center bg-gray-50">
       <nav className="w-full bg-white shadow-md">
         <div className="max-w-7xl mx-auto p-4 flex justify-between items-center">
-          <div className="text-2xl text-blue-600 font-semibold">
+          <div className="text-2xl text-blue-600 font-semibold flex items-center">
             <Image src={'/drbelissa.png'} width={200} height={50} alt="Dr. Belissa" />
           </div>
           <div className="hidden md:flex items-center space-x-4">
             <button
               onClick={handleNavigateHome}
-              className="p-2 bg-blue-600 text-white rounded-full hover:bg-blue-700"
+              className="p-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition duration-150 ease-in-out"
               aria-label="home"
             >
               <AiOutlineHome className="h-6 w-6" aria-hidden="true" />
             </button>
             <button
               onClick={handleSignOut}
-              className="p-2 bg-red-600 text-white rounded-full hover:bg-red-700"
+              className="p-2 bg-red-600 text-white rounded-full hover:bg-red-700 transition duration-150 ease-in-out"
               aria-label="sign out"
             >
               <BiUser className="h-6 w-6" aria-hidden="true" />
@@ -76,7 +77,7 @@ export default function Dashboard({ children }) {
           <div className="md:hidden">
             <button
               onClick={toggleMenu}
-              className="p-2 bg-blue-600 text-white rounded-full hover:bg-blue-700"
+              className="p-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition duration-150 ease-in-out"
             >
               <HiMenu className="h-6 w-6" aria-hidden="true" />
             </button>
@@ -89,34 +90,41 @@ export default function Dashboard({ children }) {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <h2 className="text-3xl text-blue-600 text-center mb-8">Dashboard</h2>
         <div className="flex justify-center mb-8 space-x-1.5">
-          <Link href="/dashboard/new">
-            <button
-              className="px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-md transition duration-150 ease-in-out"
-            >
-              Nouveau Patient
-            </button>
-          </Link>
-          <button
-        onClick={handleExportClick}
-        className="px-4 py-2 text-white bg-green-600 hover:bg-green-700 rounded-lg shadow-md transition duration-150 ease-in-out"
-      >
-        Exporter Données
-      </button>
-        </div>
+  <Link href="/dashboard/new">
+    <button
+      className="p-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 shadow-md transition duration-150 ease-in-out"
+      title="Nouveau Patient"
+    >
+      <AiOutlineUserAdd className="h-6 w-6" aria-hidden="true" />
+    </button>
+  </Link>
+  <button
+    onClick={handleExportClick}
+    className="p-2 bg-green-600 text-white rounded-full hover:bg-green-700 shadow-md transition duration-150 ease-in-out"
+    title="Exporter Données"
+  >
+    <FaFileDownload className="h-6 w-6" aria-hidden="true" />
+  </button>
+</div>
+
         {children}
       </motion.div>
       {isMenuOpen && (
-        <div className="p-4 bg-white rounded-md shadow-md space-y-2 md:hidden">
+        <motion.div
+          className="p-4 bg-white rounded-md shadow-md space-y-2 md:hidden"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+        >
           <button
             onClick={handleSignOut}
-            className="w-full text-left block px-3 py-2 rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-100"
+            className="w-full text-left block px-3 py-2 rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-100 transition duration-150 ease-in-out"
           >
             <FiLogOut className="inline mr-2" />
             Deconnexion
           </button>
-        </div>
+        </motion.div>
       )}
     </div>
   );
